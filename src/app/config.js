87d544814384d0c0a45939d5022c8f9c140a82eb
Dmitriy Pushkov <ezze@ezze.org>
2014-11-11 01:54:09 +0300

    config.js is formatted.

--- src/app/config.js
+++ src/app/config.js
@@ -4,8 +4,8 @@ define([], function() {
     return {
         paths: {
             easel: '../lib/easeljs/lib/easeljs-0.7.1.combined',
-                jquery: '../lib/jquery/dist/jquery',
-                lodash: '../lib/lodash/dist/lodash'
+            jquery: '../lib/jquery/dist/jquery',
+            lodash: '../lib/lodash/dist/lodash'
         },
         shim: {
             easel: {
