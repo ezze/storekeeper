09693efdb4388645692651f814e3fd4f7bb339d4
Dmitriy Pushkov <ezze@ezze.org>
2014-11-16 17:59:04 +0300

    Direction object is added.

---
+++ src/app/level/direction.js
@@ -0,0 +1,13 @@
+define([], function() {
+    'use strict';
+
+    var Direction = {
+        NONE: 'none',
+        LEFT: 'left',
+        RIGHT: 'right',
+        UP: 'up',
+        DOWN: 'down'
+    };
+
+    return Direction;
+});
