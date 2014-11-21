2f20dbb6bbeabe3a1f54275a01888f56c582c3c3
Dmitriy Pushkov <ezze@ezze.org>
2014-11-21 21:55:21 +0300

    Exception module is documented.

--- src/app/exception.js
+++ src/app/exception.js
@@ -4,6 +4,17 @@
 define([], function() {
     'use strict';
 
+    /**
+     * Exception for application's inner purposes.
+     *
+     * @param {String} message
+     * Error message.
+     *
+     * @author Dmitriy Pushkov <ezze@ezze.org>
+     * @since 0.1.0
+     * @alias module:Exception
+     * @class
+     */
     var Exception = function(message) {
         Error.apply(this, arguments);
         this.name = 'StorekeeperException';
