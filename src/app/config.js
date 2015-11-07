e638d0e6317898f604f4ffd34a18c30fcc292466
Dmitriy Pushkov <ezze@ezze.org>
2015-11-07 17:40:15 +0300

    Backbone and Marionette libraries are added.

--- src/app/config.js
+++ src/app/config.js
@@ -2,11 +2,20 @@ define([], function() {
     'use strict';
 
     return {
+        baseUrl: './app',
+        waitSeconds: 60,
+        map: {
+            '*': {
+                underscore: 'lodash'          // replacing Underscore with Lo-Dash
+            }
+        },
         paths: {
             bootstrap: '../lib/bootstrap/dist/js/bootstrap',
             easel: '../lib/easeljs/lib/easeljs-0.8.1.combined',
             jquery: '../lib/jquery/dist/jquery',
-            lodash: '../lib/lodash/dist/lodash',
+            lodash: '../lib/lodash/lodash',
+            marionette: '../lib/marionette/lib/backbone.marionette',
+            mustache: '../lib/mustache.js/mustache',
             tween: '../lib/TweenJS/lib/tweenjs-0.6.1.combined'
         },
         shim: {
@@ -15,6 +24,10 @@ define([], function() {
             },
             easel: {
                 exports: 'createjs'
+            },
+            marionette: {
+                deps: ['jquery', 'underscore', 'backbone'],
+                exports: 'Marionette'
             },
             tween: {
                 exports: 'createjs'
