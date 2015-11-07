044a91f126a86855bf45515e2568b1d762d6c212
Dmitriy Pushkov <ezze@ezze.org>
2015-11-07 21:23:08 +0300

    LESS Bower package is removed.

--- grunt/config/less.js
+++ grunt/config/less.js
@@ -1,24 +1,29 @@
-/* global module:false */
-
+/* global module: false, require: false  */
 module.exports = function() {
     'use strict';
 
-    return {
-        min: {
+    var _ = require('lodash'),
+        config = {
             options: {
-                compress: true,
-                cleancss: true,
-                strictImports: true
+                strictImports: true,
+                relativeUrls: true
+            }
+        };
+
+    _.each(['combined', 'minified'], function(version) {
+        config[version] = {
+            options: {
+                compress: version === 'minified'
             },
             files: [{
                 expand: true,
                 cwd: 'src/less',
-                src: [
-                    'storekeeper.less'
-                ],
+                src: ['storekeeper.less'],
                 dest: 'assets/css',
-                ext: '.min.css'
+                ext: (version === 'minified' ? '.min' : '') + '.css'
             }]
-        }
-    };
+        };
+    });
+
+    return config;
 };
