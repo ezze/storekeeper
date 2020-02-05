df41e122333241252b218ada206c92b2a5c2f756
Dmitriy Pushkov <ezze@ezze.org>
2020-02-06 00:33:42 +0300

    Jest is configured properly to collect test coverage.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/test'
+  ],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js'
+  ]
+};
+
+module.exports = config;
